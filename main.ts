dbf40712d08925ca23fe8e83e0f116b3715d6d24
allan79dev <82371470+allan79dev@users.noreply.github.com>
2022-06-24 16:24:09 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTT
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
     servos.P0.stop()
 })
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_EVT_ANY, function () {
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     servos.P0.run(-100)
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
